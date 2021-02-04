7936fbe056b3242f938ba07a14327ab840ad8a26
Elton Correa <elton.correa@santander.com.br>
2021-02-03 23:14:40 -0300

    adicionando esqueleto do projeto

---
+++ src/main/java/com/elton/app/validatepassword/ValidatePasswordApplication.java
@@ -0,0 +1,12 @@
+package com.elton.app.validatepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatePasswordApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ValidatePasswordApplication.class, args);
+	}
+}
